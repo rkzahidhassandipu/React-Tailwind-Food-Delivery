1d3473435c04dfc4979b7f80106477780b123b62
Zahid <rkrazzkhan01731@gmail.com>
2024-08-27 22:26:23 +0800

    Fooditem show display product section design end

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -3,18 +3,20 @@ import { assets } from '../../assets/assets'
 
 const FoodItem = ({id, name, price, description, image}) => {
   return (
-    <div>
-        <div>
-            <img src={image} alt="" />
-        </div>
-
-        <div>
-            <div>
-                <p>{name}</p>
-                <img src={assets.rating_starts} alt="" />
+    <div className='inline-block w-[23.56%] gap-10 text-center mr-5 '>
+        <div className='mt-10 transform rounded-lg  bg-white  dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg'>
+            <div className=''>
+                <img className='rounded-s-xl rounded-se-lg' src={image} alt="" />
             </div>
-            <p>{description}</p>
-            <p>$ {price}</p>
+
+            <div className=''>
+                <div>
+                    <p>{name}</p>
+                    <img src={assets.rating_starts} alt="" />
+                </div>
+                <p>{description}</p>
+                <p>$ {price}</p>
+            </div>
         </div>
     </div>
   )
