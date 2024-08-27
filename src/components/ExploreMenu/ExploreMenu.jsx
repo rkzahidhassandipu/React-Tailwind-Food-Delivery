36823c09dba02dd625ba3757da120d79ec94ba86
Zahid <rkrazzkhan01731@gmail.com>
2024-08-27 18:35:39 +0800

    Explore section design start

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {menu_list} from "../../assets/assets"
+import Slider from "./Slider"
 
 const ExploreMenu = () => {
   return (
@@ -7,18 +7,7 @@ const ExploreMenu = () => {
         <h1>Explore our menu</h1>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum atque architecto ab at similique saepe. Sed, aliquam perspiciatis. Sint quos omnis maxime. Quis aperiam nihil, laborum esse fugit in laboriosam.</p>
 
-        <div>
-            {
-                menu_list.map((item, index) => {
-                    return (
-                        <div>
-                            <img src={item.menu_image} alt="" />
-                            <p>{item.menu_name}</p>
-                        </div>
-                    )
-                })
-            }
-        </div>
+        <Slider />
     </div>
   )
 }
