02a2cbcbf4f8b483e1d522672a5ac7cbd942b403
Zahid <rkrazzkhan01731@gmail.com>
2024-08-27 21:41:01 +0800

    Fooditem show display product got section end

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const FoodItem = ({id, name, price, description, image}) => {
   return (
     <div>
+        <div>
+            <img src={image} alt="" />
+        </div>
 
+        <div>
+            <div>
+                <p>{name}</p>
+                <img src={assets.rating_starts} alt="" />
+            </div>
+            <p>{description}</p>
+            <p>$ {price}</p>
+        </div>
     </div>
   )
 }
