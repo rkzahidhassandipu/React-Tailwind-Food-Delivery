98757881955bbeed0467e6d72503863c8273a26e
Zahid <rkrazzkhan01731@gmail.com>
2024-08-27 23:57:40 +0800

    storeContext section coutin end

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,20 +1,22 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { assets } from '../../assets/assets'
+import { StoreContext } from '../../context/StoreContext';
 
 const FoodItem = ({id, name, price, description, image}) => {
-    const [itemCount, setItemCount] = useState(0)
+    const [itemCount, setItemCount] = useState(0);
+    const {cartItems, addToCart, removeFromCart} = useContext(StoreContext)
   return (
     <div className='inline-block w-[23.56%] gap-10 text-center mr-5 '>
         <div className='mt-10 transform rounded-lg  bg-white  dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg'>
             <div className=''>
                 <img className='rounded-ss-xl rounded-r-xl' src={image} alt="" />
                 {
-                    !itemCount
-                    ?<img className='absolute top-48 left-2 w-10' onClick={() => setItemCount(prev => prev+1)} src={assets.add_icon_white} /> 
+                    !cartItems[id]
+                    ?<img className='absolute top-48 left-2 w-10' onClick={() => addToCart(id)} src={assets.add_icon_white} /> 
                     : <div className='flex absolute top-48 left-2 items-center'>
-                        <img src={assets.remove_icon_red} onClick={() => setItemCount(prev=> prev-1)} alt="" />
-                        <p className='rounded-full px-5 text-center bg-white font-semibold'>{itemCount}</p>
-                        <img src={assets.add_icon_green} onClick={() => setItemCount(prev=> prev+1)} alt="" />
+                        <img src={assets.remove_icon_red} onClick={() => removeFromCart(id)} alt="" />
+                        <p className='rounded-full px-5 text-center bg-white font-semibold'>{cartItems[id]}</p>
+                        <img src={assets.add_icon_green} onClick={() => addToCart(id)} alt="" />
                     </div>
                 }
             </div>
