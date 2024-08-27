39f0598b0e52f7f1c46b6b5da4e562c3e8b2e4e9
Zahid <rkrazzkhan01731@gmail.com>
2024-08-27 23:36:28 +0800

    storeContext section start

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from '../../assets/assets'
 
 const FoodItem = ({id, name, price, description, image}) => {
+    const [itemCount, setItemCount] = useState(0)
   return (
     <div className='inline-block w-[23.56%] gap-10 text-center mr-5 '>
         <div className='mt-10 transform rounded-lg  bg-white  dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg'>
             <div className=''>
-                <img className='rounded-s-xl rounded-se-lg' src={image} alt="" />
+                <img className='rounded-ss-xl rounded-r-xl' src={image} alt="" />
+                {
+                    !itemCount
+                    ?<img className='absolute top-48 left-2 w-10' onClick={() => setItemCount(prev => prev+1)} src={assets.add_icon_white} /> 
+                    : <div className='flex absolute top-48 left-2 items-center'>
+                        <img src={assets.remove_icon_red} onClick={() => setItemCount(prev=> prev-1)} alt="" />
+                        <p className='rounded-full px-5 text-center bg-white font-semibold'>{itemCount}</p>
+                        <img src={assets.add_icon_green} onClick={() => setItemCount(prev=> prev+1)} alt="" />
+                    </div>
+                }
             </div>
 
             <div className='p-3'>
